0dca54c017dcc162e46c96807b06a723528e35e0
Vaea-lef <vaea.lefeuvre@outlook.fr>
2025-04-25 15:17:13 +1100

    MAJ

--- Code/Analyses_stage.R
+++ Code/Analyses_stage.R
@@ -24,7 +24,7 @@ library(factoextra)
 ################  ACP INFLAMMABILITE ####################
 
 # Sélectionner uniquement les colonnes des pourcentages
-colonnes_infla <- BDD_esp[, 3:6]
+colonnes_infla <- BDD_esp[, c(3:6)]
 
 # Vérifier les données
 head(colonnes_infla)
@@ -50,11 +50,15 @@ fviz_pca_biplot(res.pca, repel = TRUE)
 # Afficher l'ébouli
 fviz_screeplot(res.pca, addlabels = TRUE, ylim = c(0, 50), main="Graphique de l'ébouli")
 
-
+# Ajouter un score d'inflammabilité basé sur la coordonnée de l'axe 1 ou axe 1 et 2
+coord <- res.pca$ind$coord  # coordonnées des individus
+BDD_esp$score <- coord[, 1]  # Dim 1 = axe 1
+BDD_esp$score2 <- (0.63*coord[,1] + 0.25*coord[,2]) / 2
+View(BDD_esp)
 
 #################### ACP TRAITS ################
 # Sélectionner uniquement les colonnes des pourcentages
-colonnes_traits <- BDD_esp[,c(11,13,14,17,19)]
+colonnes_traits <- na.omit(BDD_esp[,c(7:21)])
 
 # Vérifier les données
 colonnes_traits
@@ -86,7 +90,7 @@ fviz_screeplot(res.pca, addlabels = TRUE, ylim = c(0, 50), main="Graphique de l'
 ############ ACP de tout ###########
 
 # Sélectionner uniquement les colonnes des pourcentages
-colonnes_all <- BDD_esp[,c(3,4,5,6,11,13,14,17,19)]
+colonnes_all <- BDD_esp[,c(3:21)]
 
 # Vérifier les données
 colonnes_all
@@ -111,3 +115,14 @@ fviz_pca_biplot(res.pca, repel = TRUE)
 
 # Afficher l'ébouli
 fviz_screeplot(res.pca, addlabels = TRUE, ylim = c(0, 50), main="Graphique de l'ébouli")
+
+
+
+
+
+#### PLOT ######
+plot(BDD_esp$score,BDD_esp$SLA)
+m<-lm(BT~LT,data=BDD_ech)
+summary(m)
+plot(m)
+anova(m)
